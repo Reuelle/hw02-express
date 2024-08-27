593d1cf3b576190c74d81e8b1a5d941c701504de
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-27 16:26:04 +0800

    Update errorhandler.js

--- helpers/errorhandler.js
+++ helpers/errorhandler.js
@@ -1,14 +1,7 @@
-// helpers/errorHandler.js
-
+// middlewares/errorHandler.js
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-
-  res.json({
-    message: err.message,
-    // Optionally include stack trace in development
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
+  console.error(err.stack);
+  res.status(500).json({ message: err.message });
 };
 
 module.exports = errorHandler;
